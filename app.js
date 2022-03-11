f05e0c02a7bf1ef54b3c95a259bc67a5f097e7a7
Alekususenpai <83718750+Alekususenpai@users.noreply.github.com>
2022-03-11 23:03:41 +0100

    setTimeout adjusted/disappear

--- app.js
+++ app.js
@@ -2,17 +2,19 @@ const mail = document.getElementById('mail')
 
 mail.onclick = function () {
   const tooltip = document.getElementById('tooltip');
+  tooltip.style.visibility = "visible";
   navigator.clipboard.writeText('aleksandrajovanovska218@gmail.com');
   tooltip.innerHTML = "Copied!";
-  setTimeout(() => { tooltip.innerHTML = "Copy e-mail"; }, 5000);
+  setTimeout(() => { tooltip.innerHTML = "Copy e-mail"; tooltip.style.visibility = "hidden"; }, 2000);
 };
 
 const mail2 = document.getElementById('mail2')
 
 mail2.onclick = function () {
   const tooltip2 = document.getElementById('tooltip2');
+  tooltip2.style.visibility = "visible";
   navigator.clipboard.writeText('aleksandrajovanovska218@gmail.com');
   tooltip2.innerHTML = "Copied!";
-  setTimeout(() => { tooltip2.innerHTML = "Copy e-mail"; }, 5000);
+  setTimeout(() => { tooltip2.innerHTML = "Copy e-mail"; tooltip2.style.visibility = "hidden"; }, 2000);
 };
 
