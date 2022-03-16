e58fceeab5085814faf291fe628e91b93d05ba9c
Alekususenpai <83718750+Alekususenpai@users.noreply.github.com>
2022-03-16 23:49:51 +0100

    Refactored JS / Constructor function

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
+function CopyMail(element) {
+  this.element = element;
+  element.onclick = function () {
+    const tooltip = document.getElementById('tooltip');
+    tooltip.style.visibility = "visible";
+    navigator.clipboard.writeText('aleksandrajovanovska218@gmail.com');
+    tooltip.innerHTML = "Copied!";
+    setTimeout(() => { tooltip.innerHTML = "Copy e-mail"; tooltip.style.visibility = "hidden"; }, 2000);
+  };
+}
+
 const mail = document.getElementById('mail')
-
-mail.onclick = function () {
-  const tooltip = document.getElementById('tooltip');
-  tooltip.style.visibility = "visible";
-  navigator.clipboard.writeText('aleksandrajovanovska218@gmail.com');
-  tooltip.innerHTML = "Copied!";
-  setTimeout(() => { tooltip.innerHTML = "Copy e-mail"; tooltip.style.visibility = "hidden"; }, 2000);
-};
-
 const mail2 = document.getElementById('mail2')
 
-mail2.onclick = function () {
-  const tooltip2 = document.getElementById('tooltip2');
-  tooltip2.style.visibility = "visible";
-  navigator.clipboard.writeText('aleksandrajovanovska218@gmail.com');
-  tooltip2.innerHTML = "Copied!";
-  setTimeout(() => { tooltip2.innerHTML = "Copy e-mail"; tooltip2.style.visibility = "hidden"; }, 2000);
-};
+
+const element1 = new CopyMail(mail);
+const element2 = new CopyMail(mail2);
+
+
 
