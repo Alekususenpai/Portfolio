3202d7615538eafb0d00f03d8dd0c0cb63017473
Alekususenpai <aa_lara@live.com>
2023-10-11 21:15:44 -0500

    Canvas Loader refactored

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -6,20 +6,12 @@ const CanvasLoader = () => {
     <Html
       as='div'
       center
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
-      }}
     >
-      <span className='canvas-loader'></span>
       <p
         style={{
           fontSize: 14,
-          color: "#F1F1F1",
+          color: "white",
           fontWeight: 800,
-          marginTop: 40,
         }}
       >
         {progress.toFixed(2)}%
@@ -29,3 +21,5 @@ const CanvasLoader = () => {
 };
 
 export default CanvasLoader;
+
+
